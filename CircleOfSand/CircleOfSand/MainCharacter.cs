eda4a8409bf994ea66266e7226a3e828b3bda6f7
Victor <avakado58rus@gmail.com>
2020-02-21 20:19:44 +0300

    Создан проект Моногейм, добавлен класс главного персонажа

---
+++ CircleOfSand/CircleOfSand/MainCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CircleOfSand
+{
+    class MainCharacter:DrawableGameComponent
+    {
+        public Vector2 PositionMainCharacter { get; private set; }
+
+        public MainCharacter(Game game, Texture texture): base(game)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
